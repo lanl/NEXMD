4cac6a553f49720977936b7249f549da22d79f07
Josiah Bjorgaard <jbjorgaard@lanl.gov>
2015-08-09 21:54:57 -0600

    working on nacr

--- src/sqm/nacr.F90
+++ src/sqm/nacr.F90
@@ -78,57 +78,56 @@ subroutine nacR_analytic(xyz_in, ihop, icheck)
 !	end do
 
     !KGB commented 2 lines out
-	!ihop = 2  ! initial state
-	!icheck = 1	! final state
-	
+    !ihop = 2  ! initial state
+    !icheck = 1	! final state
+   
 ! Form transition density martix between state ihop and icheck
 !  xi_ih_ic = (I-2rho)(xi_ih xi_ic + xi_ic xi_ih)
-
-	call getmodef(M2_M,Mx_M,Np,Nh,ihop,qm2ds%cmdqt,qm2ds%nacr_scratch)
-	call getmodef(M2_M,Mx_M,Np,Nh,icheck,qm2ds%cmdqt,qm2ds%eta_scratch)
-	
-	call dgemm('N','T',Nb,Nb,Nb,ff1,qm2ds%nacr_scratch,Nb,qm2ds%eta_scratch,Nb,ff0,qm2ds%eta,Nb)
-	call dgemm('T','N',Nb,Nb,Nb,ff1,qm2ds%eta_scratch,Nb,qm2ds%nacr_scratch,Nb,ff1,qm2ds%eta,Nb)
-	call Iminus2rho(Nb,Np,qm2ds%eta,qm2ds%xi)
-	call mo2sitef (Nb,qm2ds%vhf,qm2ds%xi,qm2ds%eta,qm2ds%xi_scratch)
-	
+   call getmodef(M2_M,Mx_M,Np,Nh,ihop,qm2ds%cmdqt,qm2ds%nacr_scratch)
+   call getmodef(M2_M,Mx_M,Np,Nh,icheck,qm2ds%cmdqt,qm2ds%eta_scratch)
+   call dgemm('N','T',Nb,Nb,Nb,ff1,qm2ds%nacr_scratch,Nb,qm2ds%eta_scratch,Nb,ff0,qm2ds%eta,Nb)
+   call dgemm('T','N',Nb,Nb,Nb,ff1,qm2ds%eta_scratch,Nb,qm2ds%nacr_scratch,Nb,ff1,qm2ds%eta,Nb)
+   call Iminus2rho(Nb,Np,qm2ds%eta,qm2ds%xi)
+   call mo2sitef (Nb,qm2ds%vhf,qm2ds%xi,qm2ds%eta,qm2ds%xi_scratch)
 ! Above eta contains transition density martix between state ihop and icheck in AO
+   write(6,*)'Ti->j',qm2ds%eta(1:3)
 
-	call getmodef(M2_M,Mx_M,Np,Nh,ihop,qm2ds%cmdqt,qm2ds%nacr_scratch)
-	call mo2sitef (Nb,qm2ds%vhf,qm2ds%nacr_scratch,qm2ds%xi,qm2ds%eta_scratch)
-	call getmodef(M2_M,Mx_M,Np,Nh,icheck,qm2ds%cmdqt,qm2ds%nacr_scratch)
-	call mo2sitef (Nb,qm2ds%vhf,qm2ds%nacr_scratch,qm2ds%xi_scratch_2,qm2ds%eta_scratch)
-
+   call getmodef(M2_M,Mx_M,Np,Nh,ihop,qm2ds%cmdqt,qm2ds%nacr_scratch)
+   call mo2sitef (Nb,qm2ds%vhf,qm2ds%nacr_scratch,qm2ds%xi,qm2ds%eta_scratch)
+   call getmodef(M2_M,Mx_M,Np,Nh,icheck,qm2ds%cmdqt,qm2ds%nacr_scratch)
+   call mo2sitef (Nb,qm2ds%vhf,qm2ds%nacr_scratch,qm2ds%xi_scratch_2,qm2ds%eta_scratch)
 ! Above xi and xi_scratch_2 contain transition density martices for states ihop and icheck in AO
-	ideriv = 3
-	if (ideriv.eq.3) then  ! Fast GS MOPAC derivatives
-	    do i = 1,qmmm_struct%nquant_nlink
-	        do j = 1,3
-       		    xyz(j,i) = xyz_in((i-1)*3+j)
-				dxyz1(j,i) = 0.d0
-	        end do
-	    end do
+
+   ideriv = 3
+   if (ideriv.eq.3) then  ! Fast GS MOPAC derivatives
+       do i = 1,qmmm_struct%nquant_nlink
+           do j = 1,3
+              xyz(j,i) = xyz_in((i-1)*3+j)
+              dxyz1(j,i) = 0.d0
+           end do
+       end do
 ! Term Tr(F^x rho_ij) (only symmetric part contributes)
-		call packing(Nb,qm2ds%eta,qm2ds%nacr_scratch,'s')
-		call DCART1(dxyz1,2.0*qm2_struct%den_matrix,qm2ds%nacr_scratch,xyz)
+       call packing(Nb,qm2ds%eta,qm2ds%nacr_scratch,'s')
+       call DCART1(dxyz1,qm2_struct%den_matrix,qm2ds%nacr_scratch,xyz)
 ! FIXME Why den_matrix is twice of that in old version?
+       write(6,*)'dxyz1',dxyz1(:,1)
 
 ! Convert from kcal/A to eV/A
-		do j = 3,N3,3
-			qm2ds%dij(j-2)= dxyz1(1,j/3)*KCAL_TO_EV/(qm2ds%e0(qm2ds%kx(icheck))-qm2ds%e0(qm2ds%kx(ihop)))
-			qm2ds%dij(j-1)= dxyz1(2,j/3)*KCAL_TO_EV/(qm2ds%e0(qm2ds%kx(icheck))-qm2ds%e0(qm2ds%kx(ihop)))
-			qm2ds%dij(j)  = dxyz1(3,j/3)*KCAL_TO_EV/(qm2ds%e0(qm2ds%kx(icheck))-qm2ds%e0(qm2ds%kx(ihop)))
-		end do
-	end if
+       do j = 3,N3,3
+          qm2ds%dij(j-2)= dxyz1(1,j/3)*KCAL_TO_EV/(qm2ds%e0(qm2ds%kx(icheck))-qm2ds%e0(qm2ds%kx(ihop)))
+          qm2ds%dij(j-1)= dxyz1(2,j/3)*KCAL_TO_EV/(qm2ds%e0(qm2ds%kx(icheck))-qm2ds%e0(qm2ds%kx(ihop)))
+          qm2ds%dij(j)  = dxyz1(3,j/3)*KCAL_TO_EV/(qm2ds%e0(qm2ds%kx(icheck))-qm2ds%e0(qm2ds%kx(ihop)))
+       end do
+   end if
+       write(6,*)'qm2ds%dij',qm2ds%dij(1:3)
 
-	!write(6,*)  'dij'
-	!do i = 1,Na*3
-    !write(6,*)  qm2ds%dij(i)
-	!end do
+   !write(6,*)  'dij'
+   !do i = 1,Na*3
+      !write(6,*)  qm2ds%dij(i)
+   !end do
+   !flush(6)
+   !STOP
 
-	flush(6)
-	!STOP
-
-	return
+   return
 
 end subroutine nacR_analytic
